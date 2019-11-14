88ea465b4a8edfff4267b7f9da710e3039deccf4
tati <mazzasantino1206@gmail.com>
2019-11-14 20:11:46 -0300

    Se agrego dotenv y passport a package.json

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express")
-const app = express()
 
+const app = express()
+require("dotenv").config()
+
+const routes = require("./routes/index")
+
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
